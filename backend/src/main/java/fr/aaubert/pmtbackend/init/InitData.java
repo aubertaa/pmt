a82054d48813c68c84a7e5d42dbe557421f812a0
aubertaa <aubert.aa@gmail.com>
2024-10-20 20:35:36 +0200

    add 1 user as init data

---
+++ backend/src/main/java/fr/aaubert/pmtbackend/init/InitData.java
@@ -0,0 +1,24 @@
+package fr.aaubert.pmtbackend.init;
+
+import fr.aaubert.pmtbackend.model.User;
+import fr.aaubert.pmtbackend.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class InitData implements CommandLineRunner {
+
+        private final UserRepository userRepository;
+
+        @Override
+        public void run(String... args) throws Exception {
+                //init data
+                User user = new User();
+                user.setUsername("john_doe");
+                user.setEmail("john.doe@example.com");
+                user.setPassword("securepassword");
+                userRepository.save(user);
+        }
+}
